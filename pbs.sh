841ede289e899296bc6fe3b9cfbb4c1350c3a8e9
Olaf Hering <olaf@aepfle.de>
2017-05-19 19:22:27 +0200

    pbs.sh
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- pbs.sh
+++ pbs.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-exec /usr/bin/osc --config=$HOME/.osc/oscrc -A https://pmbs-api.links2linux.org "$@"
+export OSC_CONFIG=$HOME/.osc/oscrc
+exec /usr/bin/osc --config=$OSC_CONFIG -A https://pmbs-api.links2linux.org "$@"
