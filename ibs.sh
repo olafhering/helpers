165e6620cf5b7bf8578d427f790b0aeb1a9ad1ec
Olaf Hering <olaf@aepfle.de>
2017-05-19 19:22:26 +0200

    ibs.sh
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- ibs.sh
+++ ibs.sh
@@ -1,2 +1,3 @@
 #/bin/sh
-exec /usr/bin/osc --config=$HOME/.osc/oscrc -A https://api.suse.de "$@"
+export OSC_CONFIG=$HOME/.osc/oscrc
+exec /usr/bin/osc --config=$OSC_CONFIG -A https://api.suse.de "$@"
