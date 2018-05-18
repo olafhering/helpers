c825443cd9be04f7378c81aba3aaf7fef348aaae
Olaf Hering <olaf@aepfle.de>
2018-05-18 22:40:06 +0200

    infinite zypper loop
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- zypper.sh
+++ zypper.sh
@@ -3,4 +3,4 @@ exec $(type -P env) \
 	INITRD_IN_POSTTRANS=1 \
 	$(type -P nice ) -n 19 \
 	$(type -P ionice) -c 3 \
-	$(type -P zypper) --verbose --verbose --verbose --verbose --no-refresh "$@"
+	/usr/bin/zypper --verbose --verbose --verbose --verbose --no-refresh "$@"
