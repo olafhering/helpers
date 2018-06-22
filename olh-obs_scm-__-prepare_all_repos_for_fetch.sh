8c692eca25057741c16dff1a2f58c43d94017544
Olaf Hering <olaf@aepfle.de>
2018-06-22 08:28:25 +0200

    grub.git
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-obs_scm-__-prepare_all_repos_for_fetch.sh
+++ olh-obs_scm-__-prepare_all_repos_for_fetch.sh
@@ -23,6 +23,24 @@ else
 fi
 popd > /dev/null
 #
+repo_dir=grub.git
+if ! pushd "${repo_dir}" > /dev/null
+then
+  mkdir -v "${repo_dir}"
+  pushd "${repo_dir}" > /dev/null
+fi
+if pushd .git > /dev/null
+then
+  popd > /dev/null
+else
+  git init
+  git remote add    --tags upstream            git://git.savannah.gnu.org/grub.git
+  git remote add --no-tags github_olafhering   git@github.com:olafhering/grub.git
+  git remote add --no-tags gitlab_olafhering   git@gitlab.com:olafhering/grub.git
+  git remote add --no-tags gitlab_olh          gitlab@gitlab.suse.de:olh/grub.git
+fi
+popd > /dev/null
+#
 repo_dir=ipxe.git
 if ! pushd "${repo_dir}" > /dev/null
 then
