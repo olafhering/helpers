98f326cbb8cf61091d2c10b68375c78fd187ff7b
Olaf Hering <olaf@aepfle.de>
2019-05-10 15:03:56 +0200

    ignore qemu_xen@ovmf
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-obs_scm-gitrepo-2-rpm_meta.sh
+++ olh-obs_scm-gitrepo-2-rpm_meta.sh
@@ -52,6 +52,7 @@ allow_submodule() {
     qemu_xen@ipxe) allow=false ;;
     qemu_xen@seabios) allow=false ;;
     qemu_xen@sgabios) allow=false ;;
+    qemu_xen@ovmf) allow=false ;;
     *) ;;
   esac
   if test "${allow}" = "true"
