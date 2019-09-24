ce1cbeabc8075595de48f0e300f00d363faf6ff7
Olaf Hering <olaf@aepfle.de>
2019-09-24 07:36:30 +0200

    git://git.linux-nfs.org/~bfields/linux.git
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-kerncvs-__-clone_upstream_linux_mirror.git.sh
+++ olh-kerncvs-__-clone_upstream_linux_mirror.git.sh
@@ -11,6 +11,7 @@ jirislaby.linux-stable.git|https://git.kernel.org/pub/scm/linux/kernel/git/jiris
 without_tags="
 github.olafhering.linux.git|git@github.com:olafhering/linux.git
 airlied.linux.git|git://people.freedesktop.org/~airlied/linux
+bfields.linux.git|git://git.linux-nfs.org/~bfields/linux.git
 bp.bp.git|https://git.kernel.org/pub/scm/linux/kernel/git/bp/bp.git
 davem.net-next.git|https://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git
 davem.net.git|https://git.kernel.org/pub/scm/linux/kernel/git/davem/net.git
