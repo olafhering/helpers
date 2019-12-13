b13255696922dc22f67e81269264fd0f6d4cb5e7
Olaf Hering <olaf@aepfle.de>
2019-12-13 09:32:38 +0100

    qemu stable-4.x
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-obs_scm-update_all_repos.sh
+++ olh-obs_scm-update_all_repos.sh
@@ -314,6 +314,9 @@ then
   push_master
   if ${push}
   then
+  git push github_olafhering 'refs/remotes/upstream/stable-4.*:refs/heads/stable-4.*' &> $t/qemu.github_olafhering &
+  git push gitlab_olafhering 'refs/remotes/upstream/stable-4.*:refs/heads/stable-4.*' &> $t/qemu.gitlab_olafhering &
+  git push gitlab_olh        'refs/remotes/upstream/stable-4.*:refs/heads/stable-4.*' &> $t/qemu.gitlab_olh        &
   git push github_olafhering 'refs/remotes/upstream/stable-3.*:refs/heads/stable-3.*' &> $t/qemu.github_olafhering &
   git push gitlab_olafhering 'refs/remotes/upstream/stable-3.*:refs/heads/stable-3.*' &> $t/qemu.gitlab_olafhering &
   git push gitlab_olh        'refs/remotes/upstream/stable-3.*:refs/heads/stable-3.*' &> $t/qemu.gitlab_olh        &
