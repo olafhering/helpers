043fa3a48f059f2729f70f8985b82dd2dbd7ff0c
Olaf Hering <olaf@aepfle.de>
2021-10-29 08:35:48 +0200

    mirror qemu stable-6.x
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-obs_scm-update_all_repos.sh
+++ olh-obs_scm-update_all_repos.sh
@@ -222,6 +222,9 @@ then
   push_master
   if ${push}
   then
+  git push github_olafhering 'refs/remotes/upstream/stable-6.*:refs/heads/stable-6.*' &> $t/qemu.github_olafhering &
+  git push gitlab_olafhering 'refs/remotes/upstream/stable-6.*:refs/heads/stable-6.*' &> $t/qemu.gitlab_olafhering &
+  git push gitlab_olh        'refs/remotes/upstream/stable-6.*:refs/heads/stable-6.*' &> $t/qemu.gitlab_olh        &
   git push github_olafhering 'refs/remotes/upstream/stable-5.*:refs/heads/stable-5.*' &> $t/qemu.github_olafhering &
   git push gitlab_olafhering 'refs/remotes/upstream/stable-5.*:refs/heads/stable-5.*' &> $t/qemu.gitlab_olafhering &
   git push gitlab_olh        'refs/remotes/upstream/stable-5.*:refs/heads/stable-5.*' &> $t/qemu.gitlab_olh        &
