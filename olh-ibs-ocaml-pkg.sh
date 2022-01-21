b3430fdb9bff1505dea861df6f9f91ddd703e51e
Olaf Hering <olaf@aepfle.de>
2022-01-21 21:14:12 +0100

    findlib can not be exported via git
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-ibs-ocaml-pkg.sh
+++ olh-ibs-ocaml-pkg.sh
@@ -123,7 +123,7 @@ case "${upstream}" in
 	ocaml-sha) pkg='ocaml-sha' ;;
 	ocaml-ssl) pkg='ocaml-ssl' ;;
 	ocaml-version) pkg='ocaml-version' ;;
-	ocamlfind) pkg='ocaml-findlib' ;;
+	ocamlfind) pkg='ocaml-findlib' ; echo "download tar.gz" ; exit 1 ;;
 	ocamlformat) pkg='ocaml-ocamlformat' ;;
 	ocamlgraph) pkg='ocaml-ocamlgraph' ;;
 	ocplib-endian) pkg='ocaml-ocplib-endian' ;;
