3f932153fc94c13ff276bbcdd218164edc16b515
Olaf Hering <olaf@aepfle.de>
2022-07-25 16:44:01 +0000

    really find untagged kernel-azure binary packages
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-kerncvs-find-tagged-azure-kernel-packages.sh
+++ olh-kerncvs-find-tagged-azure-kernel-packages.sh
@@ -5,33 +5,48 @@ unset ${!LC_*}
 read td < <(mktemp --directory --tmpdir=/dev/shm .XXX)
 trap "rm -rf '${td}'" EXIT
 export TMPDIR="${td}"
+declare -a tags
+declare -a top_dirs
+declare -a rpm_dirs
+top_dirs=(
+SUSE/Products/SLE-Module-Development-Tools-OBS
+SUSE/Products/SLE-Module-Public-Cloud
+SUSE/Updates/SLE-Module-Development-Tools-OBS
+SUSE/Updates/SLE-Module-Public-Cloud
+SUSE/Updates/SLE-SERVER
+)
 updates_suse_com_dir=$1
 kernel_source_git_dir=$2
 list_kernel_binaries="${td}/list_kernel_binaries"
 list_git_revision_kernel_binary="${td}/list_git_revision_kernel_binary"
-pushd "${updates_suse_com_dir}" > /dev/null
-popd > /dev/null
 pushd "${kernel_source_git_dir}" > /dev/null
 popd > /dev/null
+pushd "${updates_suse_com_dir}" > /dev/null
+for i in ${top_dirs[@]}
+do
+	rpm_dirs+=( $(ls -1d "${i}"/{12-SP5,15*}/*/product/{aarch64,x86_64} "${i}"/{12-SP5,15*}/*/update/{aarch64,x86_64} 2>/dev/null || :) )
+done
 #
-find "${updates_suse_com_dir}" -xdev -name 'kernel-azure-?.*.x86_64.rpm' > "${list_kernel_binaries}"
+echo "Searching in ${#rpm_dirs[@]} directories ..."
+find "${rpm_dirs[@]}" -xdev -name 'kernel-azure-?.*64.rpm' -exec /usr/bin/readlink -f '{}' + > "${list_kernel_binaries}"
+wc -l "${list_kernel_binaries}"
+popd > /dev/null
 #
 while read
 do
 	read rev < <(rpm -qpi "${REPLY}" | awk '/^GIT Revision:/{print $3}')
 	echo "${rev} ${REPLY}" >> "${list_git_revision_kernel_binary}"
 done < <(sort "${list_kernel_binaries}")
+wc -l "${list_git_revision_kernel_binary}"
 #
 pushd "${kernel_source_git_dir}" > /dev/null
 #
 while read rev filename
 do
-	read < <(git --no-pager log --oneline -n1 "${rev}^!")
-	case "${REPLY}" in
-	*tag:*) ;;
-	*)
+	tags=( $(git --no-pager tag --points-at "${rev}") )
+	if test ${#tags[@]} -eq 0
+	then
 		echo "${rev} ${filename}"
 		echo "${REPLY}"
-	;;
-	esac
+	fi
 done < "${list_git_revision_kernel_binary}"
