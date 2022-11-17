e2ead98139e4d20fc6c72dc83f44b1aebaa785f0
Olaf Hering <olaf@aepfle.de>
2022-11-17 14:11:52 +0000

    document environment variables
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-kerncvs-env.sh
+++ olh-kerncvs-env.sh
@@ -1,5 +1,10 @@
+# used by sequence-patch.sh
 export SCRATCH_AREA=/dev/shm/SCRATCH_AREA
+# used by sequence-patch.sh and tar-up.sh, but extracting from git appears to be faster
 export MIRROR=~/work/src/kernel/MIRROR
+# used by git_sort
 export LINUX_GIT=~/work/src/kernel/LINUX_GIT
+# used helpers.rpm
 export UPSTREAM_REPOS=~/work/src/kernel/upstream.linux
+# used helpers.rpm
 export WORK_KERNEL=~/work/src/kernel
