6fb8308647ce5639a9c9658b6976d36f119246c2
Olaf Hering <olaf@aepfle.de>
2022-11-17 10:53:15 +0000

    use option for cloning
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-kerncvs-prepare-new-workstation.sh
+++ olh-kerncvs-prepare-new-workstation.sh
@@ -91,7 +91,7 @@ as_user() {
 	fi
 
 	/usr/share/helpers/bin/olh-kerncvs-__-clone_upstream_linux_mirror.git
-	/usr/share/helpers/bin/olh-kerncvs-clone_kerncvs_kernel-source
+	/usr/share/helpers/bin/olh-kerncvs-clone_kerncvs_kernel-source -c
 }
 
 read id < <(id -u)
