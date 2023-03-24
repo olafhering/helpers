7a62ea714761cac283d726b11d3e3a672b89752b
Olaf Hering <olaf@aepfle.de>
2023-03-24 11:42:48 +0000

    record also the APIUSER in cookie filename
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- ibs.sh
+++ ibs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 APIHOST='api.suse.de'
+APIUSER='olh'
 export OSC_CONFIG=$HOME/.osc/oscrc
-export XDG_STATE_HOME="/dev/shm/.osc_cookiejar.${APIHOST}"
+export XDG_STATE_HOME="/dev/shm/.osc_cookiejar.${APIUSER}.${APIHOST}"
 rm -f ~/.osc_cookiejar ~/.local/state/osc/cookiejar
 exec /usr/bin/osc --config=$OSC_CONFIG -A "https://${APIHOST}" "$@"
