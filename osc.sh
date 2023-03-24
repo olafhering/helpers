17cf19ec6d89f449da089a83db7573da7f625cd3
Olaf Hering <olaf@aepfle.de>
2023-03-24 13:00:40 +0000

    run osc wrapper, based on _apiurl
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- osc.sh
+++ osc.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+apiurl=
+if test -f .osc/_apiurl
+then
+	read apiurl < .osc/_apiurl
+	case "${apiurl}" in
+	*//api.opensuse.org) exec obs "$@" ;;
+	*//api.suse.com) exec sbs "$@" ;;
+	*//api.suse.de) exec ibs "$@" ;;
+	*//pmbs-api.links2linux.org) exec pbs "$@" ;;
+	esac
+fi
 export OSC_CONFIG=$HOME/.osc/oscrc
 exec /usr/bin/osc --config=$OSC_CONFIG "$@"
