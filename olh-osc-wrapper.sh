de60247f2eeaa97670bbeb3e263c2f286373dcb3
Olaf Hering <olaf@aepfle.de>
2023-03-28 08:38:08 +0000

    use conf suffix for host specific oscrc
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-osc-wrapper.sh
+++ olh-osc-wrapper.sh
@@ -2,7 +2,7 @@
 APIUSER=$1
 APIHOST=$2
 shift 2
-export OSC_CONFIG=$HOME/.osc/oscrc.${APIUSER}.${APIHOST}
+export OSC_CONFIG=$HOME/.osc/oscrc.${APIUSER}.${APIHOST}.conf
 export XDG_STATE_HOME="/dev/shm/.osc_cookiejar.${APIUSER}.${APIHOST}"
 rm -f ~/.osc_cookiejar ~/.local/state/osc/cookiejar
 exec /usr/bin/osc --config=$OSC_CONFIG -A "https://${APIHOST}" "$@"
