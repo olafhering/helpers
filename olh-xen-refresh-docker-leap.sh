9954b5b82b26e5c1dd34d9fc1cfb146d578eef8f
Olaf Hering <olaf@aepfle.de>
2023-05-02 15:18:39 +0000

    pass arguments to make
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-xen-refresh-docker-leap.sh
+++ olh-xen-refresh-docker-leap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -ex
 push=
-test "$1" = 'push' && push='PUSH=1'
+declare -a make_args
+while test $# -gt 0
+do
+	case "$1" in
+	push) push='PUSH=1' ;;
+	*) make_args+=( "$1" ) ;;
+	esac
+	shift
+done
 pushd ~/git/xen.git
 git --no-pager status
 docker login registry.gitlab.com/xen-project/xen
@@ -10,6 +18,6 @@ make -C automation/build suse/opensuse-leap
 env \
 	CONTAINER_NO_PULL=1 \
 	CONTAINER=leap \
-	automation/scripts/containerize bash -exc './configure && make -j$(nproc)'
+	automation/scripts/containerize bash -exc "./configure && make ${make_args[*]} || : make failed with \$?"
 make -C automation/build suse/opensuse-leap ${push}
 echo $?
--- olh-xen-refresh-docker-tumbleweed.sh
+++ olh-xen-refresh-docker-tumbleweed.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -ex
 push=
-test "$1" = 'push' && push='PUSH=1'
+declare -a make_args
+while test $# -gt 0
+do
+	case "$1" in
+	push) push='PUSH=1' ;;
+	*) make_args+=( "$1" ) ;;
+	esac
+	shift
+done
 pushd ~/git/xen.git
 git --no-pager status
 docker login registry.gitlab.com/xen-project/xen
@@ -10,6 +18,6 @@ make -C automation/build suse/opensuse-tumbleweed
 env \
 	CONTAINER_NO_PULL=1 \
 	CONTAINER=tumbleweed \
-	automation/scripts/containerize bash -exc './configure && make -j$(nproc)'
+	automation/scripts/containerize bash -exc "./configure && make ${make_args[*]} || : make failed with \$?"
 make -C automation/build suse/opensuse-tumbleweed ${push}
 echo $?
