2b43cc901fc729aa843251209e271a7c5d75a7ea
Olaf Hering <olaf@aepfle.de>
2023-05-02 15:19:08 +0000

    work with current branch
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-xen-refresh-docker-tumbleweed.sh
+++ olh-xen-refresh-docker-tumbleweed.sh
@@ -13,7 +13,6 @@ done
 pushd ~/git/xen.git
 git --no-pager status
 docker login registry.gitlab.com/xen-project/xen
-git --no-pager checkout staging
 make -C automation/build suse/opensuse-tumbleweed
 env \
 	CONTAINER_NO_PULL=1 \
