aaca8b009f1a1441add104afdfcb0ff07f71dd38
Olaf Hering <olaf@aepfle.de>
2023-06-05 10:12:50 +0000

    olh-xen-refresh-docker-leap-clang.sh
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

---
+++ olh-xen-refresh-docker-leap-clang.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+push=
+declare -a make_args
+while test $# -gt 0
+do
+	case "$1" in
+	push) push='PUSH=1' ;;
+	*) make_args+=( "$1" ) ;;
+	esac
+	shift
+done
+pushd ~/git/xen.git
+git --no-pager status
+docker login registry.gitlab.com/xen-project/xen
+make -C automation/build suse/opensuse-leap
+env \
+	CONTAINER_NO_PULL=1 \
+	CONTAINER=leap \
+	CONTAINER_ARGS='-e CC=clang -e CXX=clang++ -e debug=n -e clang=y' \
+	automation/scripts/containerize automation/scripts/build < /dev/null
+make -C automation/build suse/opensuse-leap ${push}
+echo $?
