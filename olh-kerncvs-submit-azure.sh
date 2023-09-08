b0738e7668e573b47fef99074147a243e5f13fd7
Olaf Hering <olaf@aepfle.de>
2023-09-08 17:30:11 +0000

    handle -l and -r N options
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- olh-kerncvs-submit-azure.sh
+++ olh-kerncvs-submit-azure.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # vim: ts=2 shiftwidth=2 noexpandtab nowrap
-# Usage: $0 [-s N] [-e] DIST
+# Usage: $0 [-s N] [-e] [-l] [-r N] DIST
 # -s N will supersede an existing request, in case N is in state new|review
 # -e will submit from the *_EMBARGO branch
+# -l will just run osc log
+# -r N will submit the specified pkg revision
 set -e
 unset LANG
 unset ${!LC_*}
@@ -12,8 +14,9 @@ dist=
 pkg_rev=
 pkg_date=
 pkg_githash=
+pkg_show_log=
+pkg_rev_requested=
 declare -i supersede=0
-declare -i counter
 declare -a osc_sr_cmd
 declare -a osc_sr_cmd_args
 osc_sr_cmd_args_str=
@@ -23,6 +26,8 @@ do
 	case "$1" in
 	-s) supersede=$2 ; shift ;;
 	-e) use_embargo_branch='yes' ;;
+	-l) pkg_show_log='pkg_show_log' ;;
+	-r) pkg_rev_requested=$2 ; shift ;;
 	*)
 		if test "$#" -gt 1 
 		then
@@ -85,20 +90,36 @@ case "${update_prj}" in
 *:Update) osc_rq_type='submitrequest' ;; # maintenancerequest does not work because it lacks options understood by submit
 *) echo "Unhandled suffix for ${update_prj}" ; exit 1 ;;
 esac
+if test -n "${pkg_show_log}"
+then
+	ibs log "${src_prj}" "${pkg}"
+	exit 0
+fi
+if test -n "${pkg_rev_requested}"
+then
+	case "${pkg_rev_requested}" in
+	[0-9]*) ;;
+	r[0-9]*) pkg_rev_requested="${pkg_rev_requested:1}" ;;
+	*) echo "Unhandled pkg rev requested: ${pkg_rev_requested}" ; exit 1 ;;
+	esac
+fi
 #
 #----------------------------------------------------------------------------$
 #rN | user | YYYY-MM-DD HH:MM:SS | src_hash | a.b.c | $
 #$
 #commit git_hash$
 #----------------------------------------------------------------------------$
-counter=0
 got_line=
 got_pkg_rev=
+requested_pkg_rev_found=
 while read
 do
-	: $(( counter++ ))
 	case "${REPLY}" in
-	---------*) got_line='yes' ;;
+	---------*)
+		got_line='yes'
+		pkg_rev=
+		pkg_date=
+	;;
 	r[0-9]*)
 		if test -z "${got_line}"
 		then
@@ -110,6 +131,15 @@ do
 		pkg_rev="${values[0]}"
 		pkg_rev="${pkg_rev:1}"
 		pkg_date="${values[4]} ${values[5]}"
+		if test -n "${pkg_rev_requested}"
+		then
+			if test "${pkg_rev_requested}" = "${pkg_rev}"
+			then
+				requested_pkg_rev_found='requested_pkg_rev_found'
+			fi
+		else
+			requested_pkg_rev_found='requested_pkg_rev_found'
+		fi
 	;;
 	"")
 		if test -z "${got_line}" && test -z "${got_pkg_rev}"
@@ -119,17 +149,26 @@ do
 		fi
 	;;
 	commit*)
-		values=( ${REPLY} )
-		pkg_githash="${values[1]}"
-		break
+		if test -n "${got_line}" && test -n "${got_pkg_rev}" && test -n "${requested_pkg_rev_found}"
+		then
+			values=( ${REPLY} )
+			pkg_githash="${values[1]}"
+			break
+		fi
+		unset got_line
+		unset got_pkg_rev
+	;;
+	*)
+		unset got_line
+		unset got_pkg_rev
 	;;
 	esac
-	test ${counter} -gt 6 && break
 done < <(ibs log "${src_prj}" "${pkg}")
-if test ${counter} -gt 6
+if test -z "${pkg_githash}" || test -z "${pkg_rev}" || test -z "${pkg_date}"
 then
-	echo "Failed to parse 'osc log ${src_prj} ${pkg}':"
-	ibs log "${src_prj}" "${pkg}" | head
+	echo "Failed to parse 'ibs log ${src_prj} ${pkg}':"
+	echo "pkg_githash '${pkg_githash}' pkg_rev '${pkg_rev}' pkg_date '${pkg_date}'"
+	ibs log "${src_prj}" "${pkg}" | head -n 22
 	exit 1
 fi
 
