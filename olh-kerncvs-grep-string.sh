7f782f993fdadafef7493244de97f8a6d71dbb4f
Olaf Hering <olaf@aepfle.de>
2024-12-16 08:03:50 +0000

    olh-kerncvs-grep-string.sh
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

---
+++ olh-kerncvs-grep-string.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+renice -n 11 -p "$$"
+ionice --class 3 -p "$$"
+. /usr/share/helpers/bin/olh-kerncvs-env
+export TZ=UTC
+unset LANG
+unset ${!LC_*}
+pushd "${WORK_KERNEL}/kerncvs.kernel-source.bare.mirror"
+for string in "$@"
+do
+	for branch in ${kerncvs_active_branches_base[@]}
+	do
+		git --no-pager grep "${string}" "${branch}" || :
+	done
+done
