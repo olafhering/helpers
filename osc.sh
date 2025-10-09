43934d0b553856d4cd3368bbdc5657c5392571bc
Olaf Hering <olaf@aepfle.de>
2025-10-09 12:48:15 +0000

    adjust shell syntax
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- osc.sh
+++ osc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apiurl=
-test -z "${apiurl}" -a test -f '.osc/_apiurl' && read apiurl < "$_"
-test -z "${apiurl}" -a test -f '../.osc/_apiurl' && read apiurl < "$_"
+test -z "${apiurl}" && test -f '.osc/_apiurl' && read apiurl < "$_"
+test -z "${apiurl}" && test -f '../.osc/_apiurl' && read apiurl < "$_"
 case "${apiurl}" in
 *//api.opensuse.org) exec obs "$@" ;;
 *//api.suse.com) exec sbs "$@" ;;
