d334d0e35670e1aad6b54df78ad6eb7ae6434b2d
Olaf Hering <olaf@aepfle.de>
2025-10-09 11:55:22 +0000

    use _apiurl from prj
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- osc.sh
+++ osc.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 apiurl=
-if test -f .osc/_apiurl
-then
-	read apiurl < .osc/_apiurl
-	case "${apiurl}" in
-	*//api.opensuse.org) exec obs "$@" ;;
-	*//api.suse.com) exec sbs "$@" ;;
-	*//api.suse.de) exec ibs "$@" ;;
-	*//pmbs-api.links2linux.org) exec pbs "$@" ;;
-	esac
-fi
+test -z "${apiurl}" -a test -f '.osc/_apiurl' && read apiurl < "$_"
+test -z "${apiurl}" -a test -f '../.osc/_apiurl' && read apiurl < "$_"
+case "${apiurl}" in
+*//api.opensuse.org) exec obs "$@" ;;
+*//api.suse.com) exec sbs "$@" ;;
+*//api.suse.de) exec ibs "$@" ;;
+*//pmbs-api.links2linux.org) exec pbs "$@" ;;
+esac
 APIHOST='localhost'
 APIUSER='generic'
 exec olh-osc-wrapper "${APIUSER}" "${APIHOST}" "$@"
